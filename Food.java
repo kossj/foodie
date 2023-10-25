51e2f712eb400b6570ab98b99077227ad61b8da4
Jack K <52486731+kossj@users.noreply.github.com>
2023-10-25 15:58:41 +0000

    toString method

--- Food.java
+++ Food.java
@@ -1,33 +1,52 @@
-package Food;
-
 public class Food {
     private String name;
-    private int proteins, carbs, fats;
-    private double servings, calories;
+    private int proteins, carbs, fats, totalCalories;
+    private double servings;
 
-    public Food(String name, int proteins, int carbs, int fats, int calories, double servings) {
+    public Food(String name, int proteins, int carbs, int fats, int totalCalories, double servings) {
         this.name = name;
         this.proteins = proteins;
         this.carbs = carbs;
         this.fats = fats;
-        this.calories = calories;
+        this.totalCalories = totalCalories;
         this.servings = servings;
     }
 
-    public Food(String name, int proteins, int carbs, int fats, double servings) {
+    public Food(String name, int proteins, int carbs, int fats, int totalCalories) {
         this.name = name;
         this.proteins = proteins;
         this.carbs = carbs;
         this.fats = fats;
-        this.servings = servings;
-
-        this.calories = (proteinToCalories(proteins) + carbsToCalories(carbs) + fatsToCalories(fats)) / servings;
+        this.totalCalories = totalCalories;
+        this.servings = 1;
     }
 
-    public Food(String name, int carbs, int proteins, int fats, double calories) {}
+    public Food(String name, int proteins, int carbs, int fats) {
+        this.name = name;
+        this.proteins = proteins;
+        this.carbs = carbs;
+        this.fats = fats;
+        this.totalCalories = calculateCalories(proteins, carbs, fats);
+        this.servings = 1;
+    }
+
+    public String toString() {
+        return name + " nutrient values:\n"
+                + "\t- Calories: " + getCaloriesPerServing() +" cals\n"
+                + "\t- Servings: " + servings + "\n"
+                + "\t- Proteins " + proteins + "g\n"
+                + "\t- Carbs " + carbs + "g\n"
+                + "\t- Fats " + fats + "g\n";
+    }
 
 
-    private int proteinToCalories(int protein) { return protein * 4; }
+    private int calculateCalories(int proteins, int carbs, int fats) {
+        return proteinsToCalories(proteins) + carbsToCalories(carbs) + fatsToCalories(fats);
+    }
+
+    public double getCaloriesPerServing() { return (double)totalCalories / servings; }
+
+    private int proteinsToCalories(int proteins) { return proteins * 4; }
     private int carbsToCalories(int carbs) { return carbs * 4; }
     private int fatsToCalories(int fats) { return fats * 9; }
 }
